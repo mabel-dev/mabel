35a08fc6779699a94d2d11edd6852d7c20f43c17
Justin Joyce <justin.joyce@joocer.com>
2021-04-24 09:08:13 +0100

    [MBL-39] frame invalidation multiple days

--- mabel/data/readers/internals/base_inner_reader.py
+++ mabel/data/readers/internals/base_inner_reader.py
@@ -122,7 +122,10 @@ class BaseInnerReader(abc.ABC):
             # the .ignore file means the frame shouldn't be used 
             while any([blob for blob in blobs if as_at + '/.ignore' in blob]):
                 get_logger().debug(F".ignore file found in frame {as_at}, ignoring")
-                as_at = as_ats.pop()
+                if len(as_ats) > 0:
+                    as_at = as_ats.pop()
+                else:
+                    return []
             get_logger().debug(F"Reading from DataSet frame {as_at}")
             blobs = [blob for blob in blobs if as_at in blob]
 
--- tests/test_reader_ignore_frame.py
+++ tests/test_reader_ignore_frame.py
@@ -1,6 +1,3 @@
-"""
-Written following a bug discovered in how the Reader determines the data range.
-"""
 import datetime
 import os
 import sys
@@ -13,10 +10,12 @@ try:
 except ImportError:   # pragma: no cover
     pass
 
-DATA_DATE = datetime.date(2021,3,29)
-
 
 def test_ignore_flag():
+    """
+    test we ignore invalidated frames
+    """
+    DATA_DATE = datetime.date(2021,3,29)
     records = Reader(
             dataset='tests/data/framed',
             inner_reader=DiskReader,
@@ -24,6 +23,28 @@ def test_ignore_flag():
             end_date=DATA_DATE)
     print(next(records))
     assert next(records).get('test') == 1
+
+
+def test_ignore_flag_step_back_days():
+    """
+    test that we step back a day if all of the frames have been invalidated
+    """
+    DATA_DATE = datetime.date(2021,3,30)
+    records = Reader(
+            dataset='tests/data/framed',
+            inner_reader=DiskReader,
+            start_date=DATA_DATE,
+            end_date=DATA_DATE,
+            step_back_days=1)
+    print(next(records))
+
+
+if __name__ == "__main__":
+    test_ignore_flag()
+    test_ignore_flag_step_back_days()
+
+    print('okay')
+
 
 if __name__ == "__main__":
     test_ignore_flag()
