8c57d0093aafdfdeee719393126e3872c6f3dc5f
Justin Joyce <justin.joyce@joocer.com>
2021-06-28 17:36:56 +0100

    absolute paths reader

--- tests/test_reader_disk_reader.py
+++ tests/test_reader_disk_reader.py
@@ -15,70 +15,90 @@ traceback.install()
 
 
 def test_can_find_files():
-    """
-    ensure we can find the test files
-    the test folder has two files in it
-    """
-    # with a trailing /
-    r = DiskReader(dataset="tests/data/tweets/", raw_path=True)
-    assert len(list(r.get_list_of_blobs())) == 4  # 2 data, 2 index
+    def _inner(p):
+        """
+        ensure we can find the test files
+        the test folder has two files in it
+        """
+        # with a trailing /
+        r = DiskReader(dataset=p, raw_path=True)
+        assert len(list(r.get_list_of_blobs())) == 4  # 2 data, 2 index
 
-    # without a trailing /
-    r = DiskReader(dataset="tests/data/tweets", raw_path=True)
-    assert len(list(r.get_list_of_blobs())) == 4  # 2 data, 2 index
+        # without a trailing /
+        r = DiskReader(dataset=p, raw_path=True)
+        assert len(list(r.get_list_of_blobs())) == 4  # 2 data, 2 index
+    _inner("tests/data/tweets/")
+    _inner(os.getcwd()+"/tests/data/tweets/")
 
 
 def test_can_read_files():
-    """ensure we can read the test files"""
-    r = DiskReader(dataset="tests/data/tweets/", raw_path=True)
-    for file in [b for b in r.get_list_of_blobs() if "/_SYS." not in b]:
-        for index, item in enumerate(r.get_records(file)):
-            pass
-        assert index == 24, index
+    def _inner(p):
+        """ensure we can read the test files"""
+        r = DiskReader(dataset=p, raw_path=True)
+        for file in [b for b in r.get_list_of_blobs() if "/_SYS." not in b]:
+            for index, item in enumerate(r.get_records(file)):
+                pass
+            assert index == 24, index
+    _inner("tests/data/tweets/")
+    _inner(os.getcwd()+"/tests/data/tweets/")
 
 
 def test_only_read_selected_rows():
-    """ensure we can read the test files"""
-    r = DiskReader(dataset="tests/data/tweets/", raw_path=True)
-    for file in [b for b in r.get_list_of_blobs() if "/_SYS." not in b]:
-        for index, item in enumerate(r.get_records(file, rows=[1, 2, 3])):
-            pass
-        assert index == 2, index
+    def _inner(p):
+        """ensure we can read the test files"""
+        r = DiskReader(dataset=p, raw_path=True)
+        for file in [b for b in r.get_list_of_blobs() if "/_SYS." not in b]:
+            for index, item in enumerate(r.get_records(file, rows=[1, 2, 3])):
+                pass
+            assert index == 2, index
+    _inner("tests/data/tweets/")
+    _inner(os.getcwd()+"/tests/data/tweets/")
 
 
 def test_step_back():
-    # step back through time
-    r = Reader(
-        inner_reader=DiskReader,
-        dataset="tests/data/dated/{date}",
-        start_date=datetime.date(2021, 1, 1),
-        end_date=datetime.date(2021, 1, 1),
-        step_back_days=30,
-    )
-    assert len(list(r)) == 50
+    def _inner(p):
+        # step back through time
+        r = Reader(
+            inner_reader=DiskReader,
+            dataset=p,
+            start_date=datetime.date(2021, 1, 1),
+            end_date=datetime.date(2021, 1, 1),
+            step_back_days=30,
+        )
+        assert len(list(r)) == 50
+    _inner("tests/data/dated/{date}")
+    _inner(os.getcwd()+"/tests/data/dated/{date}")
+
 
 def test_freshness_limits():
-    # step back through time
-    r = Reader(
-        inner_reader=DiskReader,
-        dataset="tests/data/dated/{date}",
-        start_date=datetime.date(2021, 1, 1),
-        end_date=datetime.date(2021, 1, 1),
-        freshness_limit="30d",
-    )
-    assert len(list(r)) == 50
+    def _inner(p):
+        # step back through time
+        r = Reader(
+            inner_reader=DiskReader,
+            dataset=p,
+            start_date=datetime.date(2021, 1, 1),
+            end_date=datetime.date(2021, 1, 1),
+            freshness_limit="30d",
+        )
+        assert len(list(r)) == 50
+    _inner("tests/data/dated/{date}")
+    _inner(os.getcwd()+"/tests/data/dated/{date}")
+
 
 def test_step_past():
-    # step back through time
-    r = Reader(
-        inner_reader=DiskReader,
-        dataset="tests/data/dated/{date}",
-        start_date=datetime.date(2021, 1, 1),
-        end_date=datetime.date(2021, 1, 1),
-        freshness_limit="5d",
-    )
-    with pytest.raises(SystemExit):
-        assert len(list(r)) == 0
+    def _inner(p):
+        # step back through time
+        r = Reader(
+            inner_reader=DiskReader,
+            dataset=p,
+            start_date=datetime.date(2021, 1, 1),
+            end_date=datetime.date(2021, 1, 1),
+            freshness_limit="5d",
+        )
+        with pytest.raises(SystemExit):
+            assert len(list(r)) == 0
+    _inner("tests/data/dated/{date}")
+    _inner(os.getcwd()+"/tests/data/dated/{date}")
 
 
 if __name__ == "__main__":  # pragma: no cover
