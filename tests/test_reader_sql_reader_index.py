21bb55151d32c81c4622b7f145901bf38a3fc122
autoblack <cclauss@users.noreply.github.com>
2021-07-03 11:35:33 +0000

    fixup: Format Python code with Black

--- tests/test_reader_sql_reader_index.py
+++ tests/test_reader_sql_reader_index.py
@@ -11,16 +11,21 @@ traceback.install()
 
 def test_sql_uses_index(caplog):
 
-    s = SqlReader("SELECT * FROM tests.data.index.is WHERE user_name='Verizon Support'", inner_reader=DiskReader, raw_path=True)
+    s = SqlReader(
+        "SELECT * FROM tests.data.index.is WHERE user_name='Verizon Support'",
+        inner_reader=DiskReader,
+        raw_path=True,
+    )
     findings = list(s)
     assert len(findings) == 2, len(findings)
 
     if caplog:
-        has_index_log = any("Reading index from" in d for n,l,d in caplog.record_tuples)
+        has_index_log = any(
+            "Reading index from" in d for n, l, d in caplog.record_tuples
+        )
         assert has_index_log
     else:
         print("This test must be run in pytest")
-
 
 
 if __name__ == "__main__":  # pragma: no cover
