ff766ae9329417650163f51eb2b7ccc8f2f9f1ac
Justin Joyce <justin.joyce@joocer.com>
2021-07-14 22:47:07 +0100

    removing stuff

--- mabel/flows/internals/base_writer_operator.py
+++ mabel/flows/internals/base_writer_operator.py
@@ -19,26 +19,6 @@ class BaseWriterOperator(BaseOperator):
 
     def finalize(self, context: dict = None):
 
-        if isinstance(context, dict):
-
-            # if we have a profiler in the path, it will
-            profile = context.get("mabel:profile")
-            if profile:
-                timestamp = datetime.datetime.now().strftime("_SYS.as_at_%Y%m%d-%H%M%S")
-                profile_path = self.writer.dataset + timestamp + ".profile.json"
-
-                fn_bucket, fn_path, fn_stem, fn_ext = paths.get_parts(
-                    self.inner_writer.filename
-                )
-                bucket, path, stem, ext = paths.get_parts(profile_path)
-
-                if fn_bucket != bucket:
-                    profile_path = path + stem + ext
-
-                self.inner_writer.commit(
-                    json.serialize(profile, indent=True, as_bytes=True), profile_path
-                )
-
         if not context:
             context = {}
 
--- mabel/operators/__init__.py
+++ mabel/operators/__init__.py
@@ -2,5 +2,4 @@ from .end_operator import EndOperator
 from .filter_operator import FilterOperator
 from .noop_operator import NoOpOperator
 from .validator_operator import ValidatorOperator
-from .profile_data_operator import ProfileDataOperator
 from .reader_operator import ReaderOperator
