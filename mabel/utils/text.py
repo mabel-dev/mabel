44e54d08b57c607d22621c773e8574f317d38a09
autoblack <cclauss@users.noreply.github.com>
2021-07-22 13:49:55 +0000

    fixup: Format Python code with Black

--- mabel/utils/text.py
+++ mabel/utils/text.py
@@ -8,8 +8,10 @@ def tokenize(text):
     text = "".join([c for c in text if c in VALID_CHARACTERS])
     return text.split()
 
-def sanitize(text, safe_characters:str = string.ascii_letters + string.digits + ' '):
-    return ''.join([c for c in text if c in VALID_CHARACTERS])
+
+def sanitize(text, safe_characters: str = string.ascii_letters + string.digits + " "):
+    return "".join([c for c in text if c in VALID_CHARACTERS])
+
 
 def wrap_text(text, line_len):
     from textwrap import fill
