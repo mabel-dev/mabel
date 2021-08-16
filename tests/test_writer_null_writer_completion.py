4b9d6d2c8e8e4adbd7d3a2b6724aa1ee585cb037
Justin Joyce <justin.joyce@joocer.com>
2021-08-16 15:32:16 +0100

    frame complete when it's not

--- tests/test_writer_null_writer_completion.py
+++ tests/test_writer_null_writer_completion.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from mabel.adapters.disk.disk_reader import DiskReader
+from mabel.operators.reader_operator import ReaderOperator
 from mabel.adapters.null import NullWriter
 from mabel.data import BatchWriter
 from rich import traceback
@@ -43,6 +45,35 @@ def test_null_writer():
         w.append(bot)
     assert w.finalize(has_failure=True) == -1
 
+def test_timing_out_flow(caplog):
+    flow = ReaderOperator(
+            time_out=0.01,
+            inner_reader=DiskReader,
+            row_format="xml",
+            dataset="tests/data/formats/xml",
+            raw_path=True) >> EndOperator()
+
+    try:
+        with flow as runner:
+            runner(None, None, 0)
+    except:
+        pass
+
+    if caplog is None:
+        print("Test must be run in pytest")
+        return
+
+    frame_written = False
+    frame_skipped = False
+
+    for log_name, log_level, log_text in caplog.record_tuples:
+        if "Frame completion file" in log_text:
+            frame_written = True
+        if "Error found in the stack, not marking frame as complete." in log_text:
+            frame_skipped = True
+
+    assert not frame_written
+    assert frame_skipped
 
 def test_terminating_flow(caplog):
     flow = (
@@ -77,5 +108,6 @@ def test_terminating_flow(caplog):
 if __name__ == "__main__":  # pragma: no cover
     test_null_writer()
     test_terminating_flow(None)
+    test_timing_out_flow(None)
 
     print("okay")
