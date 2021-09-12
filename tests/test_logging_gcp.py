3087b195a714b65900f9c52902d755d83847496c
Justin Joyce <justin.joyce@joocer.com>
2021-09-12 16:48:58 +0100

    merge changes from 0.4

--- tests/test_logging_gcp.py
+++ tests/test_logging_gcp.py
@@ -8,14 +8,18 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], ".."))
 from mabel.logging.google_cloud_logger import GoogleLogger
 
-os.environ["PROJECT_NAME"] = "mabel"
+os.environ["IGNORE_STACKDRIVER"] = "true"
 
 
 def test_google_logging():
     """
-    This doesn't save to StackDriver - we're mainly ensuring that the redaction works
+    This doesn't save to StackDriver - we're mainly ensuring that the redaction and suppression works
     """
     assert "Noooooo" in GoogleLogger().warning("Noooooo")
+    assert "suppressed" == GoogleLogger().warning("Noooooo")
+    assert "Yess!!" in GoogleLogger().warning("Yess!!")
+    assert "suppressed" in GoogleLogger().warning("Yess!!")
+    assert "suppressed" == GoogleLogger().warning("Noooooo")
     redacted = GoogleLogger().warning({"password": "sauce"})
     assert "password" in redacted
     assert "sauce" not in redacted
