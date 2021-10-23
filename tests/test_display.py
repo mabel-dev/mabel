7b57e4a0d93f60642a08c05e85e62d88dc4da725
Justin Joyce <justin.joyce@joocer.com>
2021-10-23 19:53:34 +0100

    test table

---
+++ tests/test_display.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from mabel import Reader, DictSet
+from mabel.data import STORAGE_CLASS
+from mabel.data.internals.display import html_table
+from mabel.adapters.disk import DiskReader
+from mabel.logging import get_logger
+
+get_logger().setLevel(5)
+
+
+def get_ds(**kwargs):
+    ds = Reader(
+        inner_reader=DiskReader, dataset="tests/data/tweets", raw_path=True, **kwargs
+    )
+    return ds
+
+def test_html_table():
+
+    ds = get_ds(persistence=STORAGE_CLASS.MEMORY)
+    html = html_table(ds)
+
+    assert "<table" in html
+    assert "<th" in html
+    assert "<tr" in html
+    assert "</table>" in html
+
+if __name__ == "__main__":
+    test_html_table()
+
+    print("OKAY")
