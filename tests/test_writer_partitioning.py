a540fd067026ee1998c60fcac7b442a5ee5e784e
joocer <justin.joyce@joocer.com>
2024-05-12 03:18:49 +0100

    0.6.21

--- tests/test_writer_partitioning.py
+++ tests/test_writer_partitioning.py
@@ -11,14 +11,12 @@ from rich import traceback
 
 traceback.install()
 
-from mabel.data.writers.internals.blob_writer import BLOB_SIZE
-
 
 def test_null_writer():
     # none of these should do anything
     nw = Writer(inner_writer=NullWriter, schema=["blob"], dataset="bucket/path")
     for i in range(1):
-        nw.append({"blob": ["2"] * BLOB_SIZE})
+        nw.append({"blob": ["2"] * 10000})
     res = nw.finalize()
     assert res.startswith("NullWriter"), res
 
@@ -31,7 +29,7 @@ def test_hourly_partitions():
         partitions=["year_{yyyy}/month_{mm}/day_{dd}/by_hour/hour={HH}"],
     )
     for i in range(1):
-        nw.append({"@": [" "] * BLOB_SIZE})
+        nw.append({"@": [" "] * 10000})
     res = nw.finalize()
     assert "by_hour/hour=" in res
 
